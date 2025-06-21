1009442724fd516c16dbdcca5ae4436fba07bd4a
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-06-21 08:58:09 +0545

    completed

--- script.js
+++ script.js
@@ -2,12 +2,30 @@ const container = document.querySelector(".container")
 
 
 function createBox(){
-    for (i=0; i<50 ; i++){
-    const boxElem = document.createElement("div");
-    boxElem.classList.add("boxes");
-    boxElem.textContent="new";
-    container.append(boxElem);
+    for (let i=0; i<50 ; i++){
+       const boxElement= createElement(`Elem${i}`, i , `Element ${i}`);
+       container.append(boxElement);
+    
     }
    
+   
+}
+function createElement(symbol, atomicNumber, name){
+  
+    const boxElem = document.createElement("div");
+    boxElem.classList.add("boxes");
+    boxElem.textContent= symbol;
+    boxElem.setAttribute("data-atomic-number", atomicNumber);
+    boxElem.setAttribute("data-element-name", name);
+    
+    return boxElem;
 }
 createBox();
+function handleClick(e){
+    if (e.target.classList.contains("boxes"))
+        { 
+           alert(`Atomic Number: ${e.target.getAttribute("data-atomic-number")} and Name of Element: ${e.target.getAttribute("data-element-name")}`)
+        };
+  
+}
+container.addEventListener('click', handleClick);
